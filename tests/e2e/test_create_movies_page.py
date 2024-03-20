c14db700b9bdc25d78829e274569b2365d1d8f61
bmulli12 <bmulli12@uncc.edu>
2024-03-20 18:16:22 -0400

    Feature 02 Updated

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,25 +1,15 @@
-# TODO: Feature 2
 import pytest
-from app import create_movie
+from app import app
 from flask import Flask
 from src.repositories.movie_repository import get_movie_repository
 
 movie_repository = get_movie_repository()
 
-@pytest.fixture
-def app():
-    return Flask(__name__)
-
 def test_create_movie():
-    
-    # Create a mock request object
+    test_app = app.test_client()
 
-    
-    # Call create_movie function with mock request
-    response = movie_repository.create_movie('Test Movie', 'Test director', 10)
-    
-    # Check if the function redirects to /movies endpoint
+    response = test_app.post('/movies', data={'title': 'Test Movie', 'director': 'Test director', 'rating': 10})
+
     assert response.status_code == 302
-    assert response.location == 'http://localhost/movies'
 
-movie_repository.clear_db()
+    movie_repository.clear_db()
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,17 +1,14 @@
 # TODO: Feature 2
 import pytest
 from app import app
+from flask import Flask
+from src.repositories.movie_repository import get_movie_repository
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
 
-def test_create_movie(client):
-    # Simulate a form submission to create a movie
-    response = client.post('/movies', data=dict(
-        title='Test Movie',
-        director='Test Director',
-        rating=8
-    ), follow_redirects=True)
+movie_repository = get_movie_repository()
+
+def test_create_movie():
+
+    movie_repository.create_movie('Test Movie', 'Test Director', 10)
+    assert movie_repository.get_movie_by_title('Test Movie').title == 'Test Movie'
+    assert movie_repository.get_movie_by_title('Test Movie').director == 'Test Director'
